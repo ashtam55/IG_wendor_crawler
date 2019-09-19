5df16370f8c3feba6754e6007a30272fe3b29dd5
ashtam55 <ashtam.delhi@gmail.com>
2019-09-20 03:31:24 +0530

    hastag on firestore

--- hashtag.py
+++ hashtag.py
@@ -11,11 +11,20 @@ db = firestore.client()
 igql_api = InstagramGraphQL()
 media = []
 
-user = igql_api.get_hashtag('boxwinner')
-for media in user.recent_media():
-    print(media)
-    ref = db.collection(u'posts').set(media)
+hashtags = igql_api.get_hashtag('boxwinner')
+for media_batch in hashtags.recent_media():
+    # print(media)
+    media.extend(media_batch)
+    print(len(media_batch))
+    print("Kartik")
+    # ref = db.collection(u'posts').document().set(media)
 
+print( len(media) )
+batch = db.batch()
 
-
-
+for i in range( len(media)):
+    print (media[i]["node"]["id"])
+    print (media[i]["node"]["taken_at_timestamp"])
+    post_ref = db.collection(u'boxwinner').document(media[i]["node"]["id"])
+    batch.update(post_ref, media[i])
+batch.commit()
