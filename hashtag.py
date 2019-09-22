a43c5d655f933c668c0b7679f5b61f285b75f9a6
ashtam55 <ashtam.delhi@gmail.com>
2019-09-22 22:58:15 +0530

    listner and mqtt added

--- hashtag.py
+++ hashtag.py
@@ -6,13 +6,15 @@ from firebase_admin import db
 import instaloader
 import schedule
 import time
+import paho.mqtt.client as mqtt
 
-
+broker_address="bot.akriya.co.in"
 cred = credentials.Certificate('Social_cafe_ky.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 L = instaloader.Instaloader()
-
+client = mqtt.Client() #create new mqtt instance
+client.connect(broker_address)
 igql_api = InstagramGraphQL()
 media = []
 
@@ -22,16 +24,12 @@ def fetchHashtag():
     
     hashtags = igql_api.get_hashtag('boxwinner')
     for media_batch in hashtags.recent_media():
-        # print(media)
         media.extend(media_batch)
-        print(len(media_batch))
-        # print("Kartik")
-        # ref = db.collection(u'posts').document().set(media)
+        print("Total Tags = ",len(media_batch))
 
-        print( len(media) )
+        #Push only new hashtags to firebase
         batch = db.batch()
     for i in range( len(media)):
-        
         post_ref = db.collection(u'boxwinner').document(media[i]["node"]["id"])
         toPush = media[i]["node"]
         toPush["owner_id"] = toPush["owner"]["id"]
@@ -39,65 +37,32 @@ def fetchHashtag():
         profile = instaloader.Profile.from_id(L.context, ID)
         # print(profile.username)
         toPush["owner_username"] = profile.username
-        if (db.collection('boxwinner').document(media[i]["node"]["id"]).get().exists):
-            print(" ")
-            # batch.update(post_ref, toPush)
-        # elif(db.collection('boxwinner').document(media[i]["node"]["fullfilled"]).get().exists):
-        #     print("here")
-        else:
+        if (db.collection('boxwinner').document(media[i]["node"]["id"]).get().exists == False):
             print (media[i]["node"]["id"])
             print (media[i]["node"]["taken_at_timestamp"])
             print("pass")
             batch.set(post_ref, toPush)
-            pass
 
     batch.commit()
     media.clear()
-    # for media_batch in L.get_hashtag_posts('boxwinner'):
-    # # post is an instance of instaloader.Post
-    #     print(media)
-    #     media.extend(media_batch)
-    #     print(len(media_batch))
-    #     L.download_post(media, target='#boxwinner')
 
 
+def on_snapshot(col_snapshot, changes, read_time):
+    print(u'Callback received query snapshot.')
+    print(u'Current Content:')
+    
+    for change in changes:
+        if change.type.name == 'ADDED':
+            client.publish("VMC/1035/VEND_ORDER_ITEM",'{"cmd":"b","oiid":2,"oid":"1"}')#publish
+            print(u'New Content: {}'.format(change.document.id))
+        elif change.type.name == 'MODIFIED':
+            print(u'Modified Content: {}'.format(change.document.id))
+        elif change.type.name == 'REMOVED':
+            print(u'Removed Content: {}'.format(change.document.id))
 
-# def pushTofirebase():
-#     batch = db.batch()
+col_query = db.collection(u'boxwinner').where(u'fullfilled', u'==', True)
+query_watch = col_query.on_snapshot(on_snapshot)
 
-#     for i in range( len(media)):
-#         print (media[i]["node"]["id"])
-#         print (media[i]["node"]["taken_at_timestamp"])
-#         post_ref = db.collection(u'boxwinner').document(media[i]["node"]["id"])
-#         toPush = media[i]["node"]
-#         toPush["owner_id"] = toPush["owner"]["id"]
-#         ID = media[i]["node"]["owner"]["id"]
-#         profile = instaloader.Profile.from_id(L.context, ID)
-#         print(profile.username)
-#         toPush["owner_username"] = profile.username
-#         if (db.collection('boxwinner').document(media[i]["node"]["id"]).get().exists):
-#             print("push")
-#             batch.update(post_ref, toPush)
-#         # elif(db.collection('boxwinner').document(media[i]["node"]["fullfilled"]).get().exists):
-#         #     print("here")
-#         else:
-#             print("pass")
-#             batch.set(post_ref, toPush)
-#             pass
-        
-#     batch.commit()
-#     media.clear()
-
-# def on_snapshot(col_snapshot, changes, read_time):
-#     print(u'Callback received query snapshot.')
-#     print(u'Current cities in California:')
-#     for doc in col_snapshot:
-#         print(u'{}'.format(doc.id))
-
-# col_query = db.collection(u'boxwinner').where(u'fullfilled',u'==',u'true')
-
-# # Watch the collection query
-# query_watch = col_query.on_snapshot(on_snapshot)
 
 schedule.every(1/10).minutes.do(fetchHashtag)
 # schedule.every(1/10).minutes.do(pushTofirebase)
